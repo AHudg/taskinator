3725494322db36d1fbed014e09bad5663559e7ee
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-06-14 08:50:59 -0500

    refractored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,25 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function(event) {
     // silences browser's default settings (e.g., do not refresh the page when we submit the button)
     event.preventDefault();
     // targets the <input> element we coded in the <header> by its name = task-name
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     // targets the <select> element we coded in the <header> by its name = task-type
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    }
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
     // create DOM Element <li>
     var listItemEl = document.createElement("li");
     // styles the new <li> to match the CSS styles
@@ -21,7 +33,7 @@ var createTaskHandler = function (event) {
     // give it a class name
     taskInfoEl.classname = "task-info";
     // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     // syntax: parentNode.appendChild(childNode)
     // appends the content of the DOM element <div> to DOM element <li> we created
@@ -30,4 +42,4 @@ var createTaskHandler = function (event) {
     tasksToDoEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
